ea14809fdb2011a16aee725e051a59285da68baa
Simboi08 <simonzwang0@gmail.com>
2025-09-15 11:02:52 -0700

    2 updates

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,47 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is feature b");
+        class Monster {
+            private int health = 100;
+            private int damage = 10;
+
+            public int getDamage() {
+                return damage;
+            }
+            public void setDamage(int damage) {
+                this.damage = damage;
+            }
+            public int getHealth() {
+                return health;
+            }
+            public void setHealth(int health) {
+                this.health = health;
+            }
+
+            static void attack(Monster target) {
+
+            }
+
+            static void speak() {
+                System.out.println("Speaking");
+            }
+
+            static void die() {
+
+            }
+
+        }
+
+
+
+
+        class Vampire extends Monster {
+
+        }
+        class Dragon extends Monster {
+
+        }
+
     }
+
 }
